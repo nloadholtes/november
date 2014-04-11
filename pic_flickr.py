2ccef22bf18eda288b19f8584eff76c1880055f1
Nick Loadholtes <nick@ironboundsoftware.com>
2014-04-11 11:20:06 -0400

    Adding in the guts

--- pic_flickr.py
+++ pic_flickr.py
@@ -5,18 +5,23 @@
 #
 import flickrapi
 import sys
+import json
 
 
 def main(flickr, files):
+    print("Files: %s " % files)
     pass
 
 
 if __name__ == '__main__':
-    if len(sys.args) > 2:
-        print("Need some arguments")
+    if len(sys.argv) < 3:
+        print("Need path to the json-formatted api key/secret and then some name(s)")
         exit(-1)
     #Get API key
-    api_key = sys.argv[1]
-    flickr = flickrapi.FlickrAPI(api_key)
-    files = []
+    jdata = json.load(open(sys.argv[1]))
+    api_key = jdata.get("key")
+    api_secret = jdata.get("secret")
+    print(sys.argv)
+    flickr = flickrapi.FlickrAPI(api_key, api_secret)
+    files = sys.argv[2:]
     main(flickr, files)
